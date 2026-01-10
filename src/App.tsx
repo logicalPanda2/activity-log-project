80b0a49c77134209309ca23cb0ecd871855223ee
Marcel <marcelrg.business@gmail.com>
2026-01-10 19:17:33 +0700

    Add basic log display

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import fetchActivity from "./api/fetchActivity";
+import usePolling from "./hooks/usePolling";
+
 export default function App() {
-	return <></>;
+    const [activity, error] = usePolling<Activity>(fetchActivity, {
+        delay: 6000,
+    });
+    const [activities, setActivities] = useState<Activity[]>([]);
+
+    useEffect(() => {
+        if(!activity) return;
+
+        setActivities((prev) => [activity, ...prev]);
+    }, [activity]);
+
+	return (<>
+        {activities.length > 0 
+        ? <li>
+            {activities.map((a) => (
+                <ul key={a.id}>
+                    <p>{a.title}</p>
+                </ul>
+            ))}
+        </li> 
+        : <p>No logs available</p>}
+        {error &&
+            <p className="whitespace-pre-line">{error}</p>
+        }
+    </>);
 }
