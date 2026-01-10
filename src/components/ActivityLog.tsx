1b44b8cb2f8a605c19f2e52c96488c804464936f
Marcel <marcelrg.business@gmail.com>
2026-01-10 20:18:51 +0700

    Add load more feature

--- src/components/ActivityLog.tsx
+++ src/components/ActivityLog.tsx
@@ -5,18 +5,31 @@ export default function ActivityLog({
     activities,
     onRead,
 }: ActivityLogProps) {
-    const [isCollapsed, setCollapsed] = useState<boolean>(true);
+    const [visibleActivities, setVisibleActivities] = useState<number>(5);
+
+    const increaseVisibleActivities = (): void => {
+        setVisibleActivities(c => c + 5);
+    }
+
+    const decreaseVisibleActivities = (): void => {
+        setVisibleActivities(5);
+    }
 
     return (
         <div>
             <li>
-                {activities.map((a) => (
-                    <ActivityItem activity={a} onRead={onRead} />
-                ))}
+                {activities.map((a, index) => {
+                    if(index > (visibleActivities - 1)) return;
+
+                    return <ActivityItem activity={a} onRead={onRead} />
+                })}
             </li>
-            {isCollapsed
-            ? <button>Load more</button>
-            : <button>Collapse</button>}
+            {/* still dont understand this, refactor later */}
+            {activities.length > visibleActivities
+            ? <button onClick={increaseVisibleActivities}>Load more</button>
+            : activities.length >= visibleActivities 
+            ? <button onClick={decreaseVisibleActivities}>Collapse</button>
+            : <></>}
         </div>
     );
 }
