64baff9c4138e0ed67de6851cdd56c8eaa3510d2
Marcel <marcelrg.business@gmail.com>
2026-01-11 14:46:39 +0700

    Return refresh and pause function

--- src/hooks/usePolling.tsx
+++ src/hooks/usePolling.tsx
@@ -9,39 +9,43 @@ export default function usePolling<T>(
         delay?: number,
         maxRetries?: number,
     } = {}
-): [T | null, string] {
+): {
+    data: T | null,
+    error: string,
+    refresh: () => Promise<void>,
+    pauseAndResume: () => Promise<void>,
+} {
     const [data, setData] = useState<T | null>(null);
     const [error, setError] = useState<string>("");
     let isFetching = false;
-
-    useEffect(() => {
-        let timeoutId: number;
-        let errors: number = 0;
+    let timeoutId: number;
+    let errors: number = 0; 
         
-        const tick = async () => {
-            if(isFetching) return;
+    const tick = async () => {
+        if(isFetching) return;
 
-            setError("");
-            isFetching = true;
+        setError("");
+        isFetching = true;
 
-            try {
-                const result = await fetchFn();
-                setData(result);
-                isFetching = false;
-            } catch(error) {
-                errors++;
-                if(hasStringMessage(error)) setError(`Error: ${error.message}\nRetrying...`);
-                isFetching = false;
-            }
-
-            timeoutId = setTimeout(tick, delay);
-
-            if(errors >= maxRetries) {
-                clearTimeout(timeoutId);
-                setError(`Max retries exceeded. Please click refresh to rerun the auto-refresh.`);
-            }
+        try {
+            const result = await fetchFn();
+            setData(result);
+            isFetching = false;
+        } catch(error) {
+            errors++;
+            if(hasStringMessage(error)) setError(`Error: ${error.message}\nRetrying...`);
+            isFetching = false;
         }
 
+        timeoutId = setTimeout(tick, delay);
+
+        if(errors >= maxRetries) {
+            clearTimeout(timeoutId);
+            setError(`Max retries exceeded. Please click refresh to rerun the auto-refresh.`);
+        }
+    }
+
+    useEffect(() => {
         if(!isFetching) tick();
 
         return () => {
@@ -49,7 +53,12 @@ export default function usePolling<T>(
         }
     }, [delay, maxRetries, fetchFn]);
 
-    return [data, error];
+    return {
+        data, 
+        error, 
+        refresh: tick, 
+        pauseAndResume: tick,
+    };
 }
 
 function hasStringMessage(error: unknown): error is Record<"message", string> {
