0334a0fa36a978c668bc8d56c710771a9d520be9
Marcel <marcelrg.business@gmail.com>
2026-01-11 18:20:57 +0700

    Adjust styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ export default function Button({
     return (
         <button
             onClick={onClick}
-            className="px-4 py-1.5 border border-solid border-black rounded mx-2 bg-neutral-100 hover:bg-neutral-100/75 active:bg-neutral-200 transition"
+            className="px-4 py-1.5 border border-solid border-black rounded mx-2 bg-neutral-50 hover:bg-neutral-100 active:bg-neutral-200 transition"
         >
             {text}
         </button>
