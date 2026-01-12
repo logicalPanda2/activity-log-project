adb2dc5e9ced4b31accc2f5599b415ac962b5843
Marcel <marcelrg.business@gmail.com>
2026-01-12 16:22:21 +0700

    Rework style

--- src/components/ActivityItem.tsx
+++ src/components/ActivityItem.tsx
@@ -1,13 +1,13 @@
 export default function ActivityItem({activity, onRead}: ActivityItemProps) {
     return (
-        <li key={activity.id} className="p-4 relative" onClick={() => onRead(activity.id)}>
+        <li key={activity.id} className="pb-4 relative border-b border-solid border-b-black" onClick={() => onRead(activity.id)}>
             <p>{activity.title}</p>
             <div className="flex flex-row justify-between mt-4">
                 <p className="text-xs">{activity.creator}</p>
                 <p className="text-xs">{activity.formattedCreationTime}</p>
             </div>
             {activity.status === "UNREAD"
-            ? <div className="absolute top-6 right-6 w-3 h-3 bg-blue-600 rounded-full"></div>
+            ? <div className="absolute top-2 right-2 w-3 h-3 bg-blue-600 rounded-full"></div>
             : <></>}
         </li>
     );
--- src/components/ActivityLog.tsx
+++ src/components/ActivityLog.tsx
@@ -9,7 +9,7 @@ export default function ActivityLog({
     const [visibleActivities, setVisibleActivities] = useState<number>(defaultVisible);
     const [topOffset, setTopOffset] = useState<number | undefined>(undefined);
     const mainDiv = useRef<HTMLDivElement | null>(null);
-    const itemHeightPx = 80;
+    const itemHeightPx = 85;
 
     const loadMore = (): void => {
         setVisibleActivities(v => v + 5);
@@ -30,8 +30,8 @@ export default function ActivityLog({
     }
 
     return (
-        <div className="rounded-lg overflow-x-hidden [scrollbar-color:gray_transparent] [scrollbar-width:thin] h-96 max-h-96 w-1/2 min-w-xl border border-solid border-black flex flex-col justify-start relative" ref={mainDiv} onScroll={() => setTopOffset(mainDiv.current?.scrollTop)}>
-            <ul>
+        <div className="rounded-lg overflow-x-hidden [scrollbar-color:gray_transparent] [scrollbar-width:thin] h-96 max-h-96 w-1/2 min-w-xl border border-solid border-black flex flex-col justify-start relative p-4" ref={mainDiv} onScroll={() => setTopOffset(mainDiv.current?.scrollTop)}>
+            <ul className="flex flex-col gap-4 mb-4">
                 {activities.map((a, index) => {
                     if(index > (visibleActivities - 1)) return;
 
@@ -41,13 +41,16 @@ export default function ActivityLog({
             <div className="flex flex-row">
                 {activities.length > defaultVisible
                 ? visibleActivities < activities.length
-                ? <button onClick={loadMore} className="m-4">Load more</button>
-                : <button onClick={resetVisibleActivities} className="m-4">Collapse</button>
+                ? <button onClick={loadMore}>Load more</button>
+                : <button onClick={resetVisibleActivities}>Collapse</button>
                 : <></>}
             </div>
             <div className="h-0 sticky bottom-0 flex flex-row justify-center">
                 { topOffset && topOffset >= itemHeightPx
-                ? <button onClick={scrollToTop} className="absolute bottom-4 overflow-visible inline-block w-8 h-8 border border-solid border-black rounded-full">^</button>
+                ? <button onClick={scrollToTop} className="absolute bottom-4 overflow-visible w-8 h-8 border border-solid border-black rounded-full bg-neutral-100">
+                    <span className="inline-block transform-[scaleX(1.5)] relative left-[2.5px] -top-0.75">^</span>
+                    <span className="inline-block transform-[scaleY(0.75)] relative -left-1.25 -top-0.5">|</span>
+                </button>
                 : <></>}
             </div>
         </div>
