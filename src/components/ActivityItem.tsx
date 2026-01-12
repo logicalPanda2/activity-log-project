9bb2eb0d9508642add8156c853f8129e3c899380
Marcel <marcelrg.business@gmail.com>
2026-01-12 07:56:20 +0700

    Rework layout and style

--- src/components/ActivityItem.tsx
+++ src/components/ActivityItem.tsx
@@ -1,9 +1,14 @@
 export default function ActivityItem({activity, onRead}: ActivityItemProps) {
     return (
-        <li key={activity.id}>
+        <li key={activity.id} className="p-4 relative" onClick={() => onRead(activity.id)}>
             <p>{activity.title}</p>
-            <p>{activity.status}</p>
-            <button onClick={() => onRead(activity.id)}>Mark as read</button>
+            <div className="flex flex-row justify-between mt-4">
+                <p className="text-xs">{activity.creator}</p>
+                <p className="text-xs">{activity.formattedCreationTime}</p>
+            </div>
+            {activity.status === "UNREAD"
+            ? <div className="absolute top-6 right-6 w-3 h-3 bg-blue-600 rounded-full"></div>
+            : <></>}
         </li>
     );
 }
